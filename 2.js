cb9fb014c7f3fc1f2a289e350d3625a4408b6a6b
perf-epam <32515520+perf-epam@users.noreply.github.com>
2020-04-20 23:49:14 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,9 +1,11 @@
-refs: [
-   {
-      ref: 'mySecondComponentReference',
-      selector: 'field[itemId=mySecondCmp]'
-   }, {
-      ref: 'anotherComponentRef',
-      selector: 'field[itemId=anotherCmp]'
-   }
-]
+var data = {
+  "key": "value",
+  "1": "value",
+  "key": "value", // Noncompliant - duplicate of "key"
+  'key': "value", // Noncompliant - duplicate of "key"
+  key: "value", // Noncompliant - duplicate of "key"
+  \u006bey: "value", // Noncompliant - duplicate of "key"
+  "\u006bey": "value", // Noncompliant - duplicate of "key"
+  "\x6bey": "value", // Noncompliant - duplicate of "key"
+  1: "value" // Noncompliant - duplicate of "1"
+}
